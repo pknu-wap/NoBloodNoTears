6d9de89a7340d6b55883274be494a6a1470e3333
dokdokorea <7dpdud@naver.com>
2019-01-20 17:58:05 +0900

    synchronize the textarea

--- src/newDocument/UploadFile.js
+++ src/newDocument/UploadFile.js
@@ -4,26 +4,36 @@ import './UploadFile.css'
 class UploadFile extends Component{
 constructor(props){
     super(props);
-    this.state={name:""};
+    this.state={
+        name:'',
+        content:""
+    }
+
 }
 
+handleChange=(e)=>{
+    this.setState({
+        content: e.target.value
+    })
+}
     render(){
+    
+        const CHANGE_REDIRECT_DOCUMENT_URL="https://forum.theseed.io/topic/232/%EC%9D%98%EA%B2%AC%EC%88%98%EB%A0%B4-%EB%A6%AC%EB%8B%A4%EC%9D%B4%EB%A0%89%ED%8A%B8-%EB%AC%B8%EB%B2%95-%EB%B3%80%EA%B2%BD";        
         return(
           <div>
               <h1 className="MainTitle">{this.state.name}(새 문서 생성)</h1>
-              <p>
-              <button>역사</button>
-              <button>삭제</button>
-              <button>이동</button>
-              </p>
+            
+              <button id="deleteBtn">삭제</button>
+              <br/>
 
-              <a  href="https://forum.theseed.io/topic/232/%EC%9D%98%EA%B2%AC%EC%88%98%EB%A0%B4-%EB%A6%AC%EB%8B%A4%EC%9D%B4%EB%A0%89%ED%8A%B8-%EB%AC%B8%EB%B2%95-%EB%B3%80%EA%B2%BD">
-              [의견수렴] 리다이렉트 문법 변경</a><br/>
+              <a href={CHANGE_REDIRECT_DOCUMENT_URL}>[의견수렴] 리다이렉트 문법 변경</a>
+              <br/>
 
               <button id="redirect">편집</button>
               <button>미리보기</button>
               <br/>
-              <textarea className="WriteContext"/>
+              <textarea className="WriteContext" value={this.state.content} onChange={this.handleChange}/>
+              <textarea value={this.state.content}></textarea> 
 
               <div id="summary">요약<br/></div>
               <input id="summaryInput"/>
@@ -33,6 +43,7 @@ constructor(props){
 
           </div>
         );
+        
     }
 }
 export default UploadFile;
