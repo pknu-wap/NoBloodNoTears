4002b2d90c189bd72e208827cab63adee107b2ad
dl57934 <dl57934@pukyong.ac.kr>
2019-02-07 18:37:23 +0900

    sign up sign in function

--- backEnd/javascript/resolvers.js
+++ backEnd/javascript/resolvers.js
@@ -1,7 +1,28 @@
 const resolvers = {
   Query: {
     firstSchema: () => "hi GraphQL + Node.js"
+  },
+  Mutation: {
+    SignUp: (_, { name, id, password, email }) =>
+      signUp({ name, id, password, email }),
+    SignIn: (_, { id, password }) => signIn({ id, password })
   }
+};
+
+const signUp = ({ name, id, password, email }) => {
+  console.log({ name, id, password, email });
+  return {
+    message: "부경위키에 오신 것을 환영합니다.",
+    success: true
+  };
+};
+
+const signIn = ({ id, password }) => {
+  console.log({ id, password });
+  return {
+    message: "부경위키에 오신 것을 환영합니다.",
+    success: true
+  };
 };
 
 export default resolvers;
