70722245b3b0f64eb59be7dc80a0a6aba9fb7ef2
dl57934 <dl57934@pukyong.ac.kr>
2019-02-07 17:22:34 +0900

    init Setting

--- backEnd/index.js
+++ backEnd/index.js
@@ -1,7 +1,9 @@
 import { GraphQLServer } from "graphql-yoga";
+import resolvers from "./javascript/resolvers";
 
-const Server = GraphQLServer({
-  type
+const Server = new GraphQLServer({
+  typeDefs: "./graphql/schema.graphql",
+  resolvers: resolvers
 });
 
 Server.start(() => console.log("GraphQL Yoga Start"));
---
+++ backEnd/javascript/resolvers.js
@@ -0,0 +1,7 @@
+const resolvers = {
+  Query: {
+    firstSchema: () => "hi GraphQL + Node.js"
+  }
+};
+
+export default resolvers;
